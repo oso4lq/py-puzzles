33b6a5ff8e552e08ecedb36903793fe11da4541c
Michael Zorin <michael.zorin.rd@gmail.com>
2024-10-16 19:37:41 +0200

    santa script

---
+++ src/task1/script.py
@@ -0,0 +1,35 @@
+# Script to run the calculation
+# python src\\task1\\script.py
+
+import os
+
+# Set the correct path for data.txt - used in every file for covenience
+script_dir = os.path.dirname(os.path.abspath(__file__))
+data_file_path = os.path.join(script_dir, 'data.txt')
+try:
+    with open(data_file_path, 'r') as file:
+        instructions = file.read()
+except FileNotFoundError:
+    print(f"no data txt file")
+    exit(1)
+
+
+# Set initial values
+x, y = 0, 0
+visited_houses = set()
+visited_houses.add((x, y))
+
+# Calculate the number of unique houses visited
+for move in instructions:
+    if move == '^':
+        y += 1
+    elif move == 'v':
+        y -= 1
+    elif move == '>':
+        x += 1
+    elif move == '<':
+        x -= 1
+    visited_houses.add((x, y))
+
+# Output
+print(len(visited_houses))

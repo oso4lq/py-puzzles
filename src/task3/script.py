221dcba9124581823aa38fe6684c12a0bcb4025c
Michael Zorin <michael.zorin.rd@gmail.com>
2024-10-17 16:17:10 +0200

    wires script

--- src/task3/script.py
+++ src/task3/script.py
@@ -18,9 +18,46 @@ except FileNotFoundError:
 wires = {}
 computed = {}
 
+# Follow instructions
 for line in instructions:
     lhs, rhs = line.split('->')
     lhs = lhs.strip()
     rhs = rhs.strip()
     wires[rhs] = lhs
 
+def evaluate(wire):
+    
+    operation = wires[wire]
+    tokens = operation.split()
+
+    if wire.isdigit():
+        return int(wire)
+    
+    if wire in computed:
+        return computed[wire]
+    
+    if len(tokens) == 1:
+        value = evaluate(tokens[0])
+
+    elif len(tokens) == 2:
+        op, x = tokens
+        if op == 'NOT':
+            value = ~evaluate(x) & 0xFFFF
+            
+    elif len(tokens) == 3:
+        x, op, y = tokens
+        if op == 'AND':
+            value = evaluate(x) & evaluate(y)
+        elif op == 'OR':
+            value = evaluate(x) | evaluate(y)
+        elif op == 'LSHIFT':
+            value = (evaluate(x) << int(y)) & 0xFFFF
+        elif op == 'RSHIFT':
+            value = evaluate(x) >> int(y)
+
+    computed[wire] = value
+    return value
+
+signal_a = evaluate('a')
+
+print(signal_a)
